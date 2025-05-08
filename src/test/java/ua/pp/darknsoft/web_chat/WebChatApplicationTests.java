a9cb263ed336635a0d595ddd1d9f4b8a0f79f634
andrewdark <samsonov.a@ukr.net>
2025-05-08 17:09:14 +0300

    Create spring-b00t app

---
+++ src/test/java/ua/pp/darknsoft/web_chat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.pp.darknsoft.web_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
