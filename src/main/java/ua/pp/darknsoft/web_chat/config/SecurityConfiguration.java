c29b5117f89cccf1fd1b90d6829e45136f4d0a3d
andrewdark <samsonov.a@ukr.net>
2025-05-08 18:21:13 +0300

    add some test config

--- src/main/java/ua/pp/darknsoft/web_chat/config/SecurityConfiguration.java
+++ src/main/java/ua/pp/darknsoft/web_chat/config/SecurityConfiguration.java
@@ -1,2 +1,36 @@
-package ua.pp.darknsoft.web_chat.config;public class SecurityConfiguration {
+package ua.pp.darknsoft.web_chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@ComponentScan("ua.pp.darknsoft.web_chat")
+public class SecurityConfiguration {
+    private static final String[] WHITE_LIST_URL = {"/api/auth/**", "/", "/error", "/favicon.ico", "/manifest.json", "/#/**", "/index", "/static/**"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+
+
+        return http.build();
+    }
 }
--- src/main/java/ua/pp/darknsoft/web_chat/controller/MainController.java
+++ src/main/java/ua/pp/darknsoft/web_chat/controller/MainController.java
@@ -1,2 +1,16 @@
-package ua.pp.darknsoft.web_chat.controller;public class MainController {
+package ua.pp.darknsoft.web_chat.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ClassPathResource homePage() {
+        return new ClassPathResource("build/index.html");
+    }
 }
